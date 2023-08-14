bf9f8340b6e90300ac8d52ff6353f941fa55c377
ktamido <ng3rdstmadgke@gmail.com>
2023-08-15 01:18:36 +0900

    とりあえずコピー

---
+++ src/digit_display/docs/README.md
@@ -0,0 +1,14 @@
+
+
+今回は7セグ表示機で遊んでみたいと思う
+
+# ■ 利用する部品
+
+## OSL40562-LRA 
+
+[OSL40562-LRA データシート](https://akizukidenshi.com/download/OSL40562-LRA.pdf)
+
+### 基本情報
+
+- 順電圧(VF) : 2.1V
+- 順電流(IF) (絶対最大定格): 20mA
---
+++ src/digit_display/temp_display.py
@@ -0,0 +1,92 @@
+from concurrent.futures import ThreadPoolExecutor
+import time
+import pigpio
+
+SEG_SHAPE = {
+    # g -> aの順
+    "0": 0b0111111, 
+    "1": 0b0000110,
+    "2": 0b1011011,
+    "3": 0b1001111,
+    "4": 0b1100110,
+    "5": 0b1101101,
+    "6": 0b1111101,
+    "7": 0b0000111,
+    "8": 0b1111111,
+    "9": 0b1101111,
+    "-": 0b1000000,
+}
+
+# 7セグ表示に利用するGPIO (a -> gの順)
+SEG_GPIO = [21, 22, 23, 24, 25, 26, 27]
+# 表示する桁を制御するGPIO (4桁目 -> 1桁目 の順)
+DIGIT_GPIO = [20, 19, 18, 17]
+
+def init_gpio(pi):
+    """gpioをリセットす関数"""
+    for gpio in SEG_GPIO:
+        pi.write(gpio, 0)
+    for gpio in DIGIT_GPIO:
+        pi.write(gpio, 1)
+
+
+def display(pi, data: list[str]):
+    """ダイナミック制御で4桁の7セグを表示する関数"""
+    while True:
+        for digit, n in enumerate(data):
+            pi.write(DIGIT_GPIO[digit], 0)
+            for i in range(0, 7):
+                pi.write(SEG_GPIO[i], (SEG_SHAPE[n] >> i) & 1)
+            time.sleep(0.001)
+
+            # リセット
+            for i in SEG_GPIO:
+                pi.write(i, 0)
+
+            # 消灯
+            pi.write(DIGIT_GPIO[digit], 1)
+
+
+def refresh(n: int, data: list[str]):
+    """表示する数字を更新する関数"""
+    new_data = [s for s in str(n).zfill(4)]
+    for i in range(0, 4):
+       data[i] = new_data[i]
+
+
+def counter(data: list[str]):
+    """表示する数字をインクリメントする関数"""
+    cnt = 0
+    while cnt < 1000:
+        time.sleep(0.1)
+        cnt = cnt + 1
+        refresh(cnt, data)
+
+def main():
+
+    pi = pigpio.pi()
+    if not pi.connected:
+        raise Exception("pigpio connection faild...")
+
+    # すべてのGPIOをOUTPUTに設定
+    for gpio in SEG_GPIO + DIGIT_GPIO:
+        pi.set_mode(gpio, pigpio.OUTPUT)
+
+    # GPIOの初期化
+    init_gpio(pi)
+    data = ["0", "0", "0", "0"]
+    # カウンターと表示は別スレッドで動かす
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        display_future = executor.submit(display, pi, data)
+        counter_future = executor.submit(counter, data)
+        try:
+            counter_future.result()
+            display_future.result()
+        finally:
+            init_gpio(pi)
+            pi.stop()
+            print("[INFO] GPIO close.")
+
+
+if __name__ == "__main__":
+    main()
