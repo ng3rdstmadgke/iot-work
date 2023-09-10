cd1a94171ec952967929ec332cba139893844169
ktamido <ng3rdstmadgke@gmail.com>
2023-09-10 16:28:38 +0900

    wip: bme280

--- src/bme280/bme280.py
+++ src/bme280/bme280.py
@@ -8,53 +8,45 @@ def int_to_binary(n: int, bits: int = 8):
 def bytes_to_binary(data: Union[bytearray,bytes]):
     return ','.join([int_to_binary(byte) for byte in data])
 
-def write(pi, spi_handler, register: int, data: int):
-    register_bytes = (register & 0b01111111).to_bytes(1, "big")
-    cnt, read_data = pi.spi_xfer(spi_handler, register_bytes)
-    print(f"register_bytes: register={int_to_binary(register)}, cnt={cnt}, read_data={int_to_binary(int.from_bytes(read_data, 'big'))}")
-    data_bytes = data.to_bytes(1, "big")
-    cnt, read_data = pi.spi_xfer(spi_handler, data_bytes)
-    print(f"register_bytes: data={int_to_binary(data)}, cnt={cnt}, read_data={int_to_binary(int.from_bytes(read_data, 'big'))}")
 
-#def read(pi, spi_handler, reg_address: int , num_bytes: int):
-#    data = []
-#    addr = reg_address | 0b10000000;
-#    pi.spi_xfer(spi_handler, addr)
-#    spi_handler()
-#    SPI.transfer(addr);
-#    for (int i = 0; i < numBytes; i++) {
-#        data[i] = SPI.transfer(0x00);
-#    }
+def write_register(pi, spi_handler, register_addr: int, data: int):
+    # 書込み時のregister指定は最上位ビットを0にする
+    write_data = (register_addr & 0b01111111) << 8 | data
+    write_data = write_data.to_bytes(2, "big")
+    pi.spi_xfer(spi_handler, write_data)
+
+def read_register(pi, spi_handler, register_addr: int, num_bytes: int) -> int:
+    # 読込み時のregister指定は最上位ビットを1にする
+    write_data = (register_addr | 0b10000000) << (8 * num_bytes)
+    write_data = write_data.to_bytes(num_bytes + 1, "big")
+    cnt, read_data = pi.spi_xfer(spi_handler, write_data)
+    print(f"cnt: {cnt}, read_data: {bytes_to_binary(read_data)}")
+    value = int.from_bytes(read_data[1:1 + num_bytes], "big")
+    return value
+
 
 def main(pi, spi_handler):
+    # 湿度の設定
+    osrs_h = 1    # Humidity oversampling x 1
+    ctrl_hum_reg  = osrs_h
+    write_register(pi, spi_handler, 0xF2, ctrl_hum_reg)
+
+    # 温度と気圧の設定
     osrs_t = 1    # Temperature oversampling x 1
     osrs_p = 1    # Pressure oversampling x 1
-    osrs_h = 1    # Humidity oversampling x 1
     mode = 3      # Normal mode
+    ctrl_meas_reg = (osrs_t << 5) | (osrs_p << 2) | mode
+    write_register(pi, spi_handler, 0xF4, ctrl_meas_reg)
+
+    # その他の設定
     t_sb = 5      # Tstandby 1000ms
     filter = 0    # Filter off 
     spi3w_en = 0  # 3-wire SPI Disable
-
-    ctrl_meas_reg = (osrs_t << 5) | (osrs_p << 2) | mode
     config_reg    = (t_sb << 5) | (filter << 2) | spi3w_en
-    ctrl_hum_reg  = osrs_h
-
-    pi.write(spi_handler, 0xF2, ctrl_hum_reg)
-    pi.write(spi_handler, 0xF4, ctrl_meas_reg)
-    pi.write(spi_handler, 0xF5, config_reg)
-
+    write_register(pi, spi_handler, 0xF5, config_reg)
     while True:
-        write_data = 0b11111010 << 8
-        write_data = write_data.to_bytes(2, "big")
-        cnt, read_data = pi.spi_xfer(spi_handler, write_data)
-        if cnt != 2:
-            print("[error] skip.")
-            time.sleep(1)
-            continue
-        value = int.from_bytes(read_data, "big") & 0b1111111111  # 10ビットを値として取り出す
-        print(f"w: {bytes_to_binary(write_data)}")
-        print(f"r: {bytes_to_binary(read_data)}")
-        print(f"value: {value}")
+        result = read_register(pi, spi_handler, 0xFA, 3)
+        print(f"temp: {int_to_binary(result)}")
         time.sleep(1)
 
 
