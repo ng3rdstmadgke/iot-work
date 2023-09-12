caf46f7756f8eea1084140bd75cb61ed305303b4
ktamido <ng3rdstmadgke@gmail.com>
2023-09-12 12:48:20 +0900

    温度が測れるようになった

--- src/bme280/bme280.py
+++ src/bme280/bme280.py
@@ -1,6 +1,14 @@
 import time
 from typing import Union
 import pigpio
+from pprint import pprint
+from collections import OrderedDict
+import enum
+
+class BitMask(int, enum.Enum):
+    SHORT = 0b1111111111111111
+    INT = 0b11111111111111111111111111111111
+    LONG = 0b1111111111111111111111111111111111111111111111111111111111111111
 
 def int_to_binary(n: int, bits: int = 8):
     return ''.join([str(n >> i & 1 ) for i in reversed(range(0, bits))])
@@ -13,16 +21,97 @@ def write_register(pi, spi_handler, register_addr: int, data: int):
     # 書込み時のregister指定は最上位ビットを0にする
     write_data = (register_addr & 0b01111111) << 8 | data
     write_data = write_data.to_bytes(2, "big")
-    pi.spi_xfer(spi_handler, write_data)
+    cnt, read_data = pi.spi_xfer(spi_handler, write_data)
+    print(f"cnt={cnt}, read_data={bytes_to_binary(read_data)}")
 
-def read_register(pi, spi_handler, register_addr: int, num_bytes: int) -> int:
+def read_register(pi, spi_handler, register_addr: int, num_bytes: int) -> bytes:
     # 読込み時のregister指定は最上位ビットを1にする
     write_data = (register_addr | 0b10000000) << (8 * num_bytes)
     write_data = write_data.to_bytes(num_bytes + 1, "big")
     cnt, read_data = pi.spi_xfer(spi_handler, write_data)
-    print(f"cnt: {cnt}, read_data: {bytes_to_binary(read_data)}")
-    value = int.from_bytes(read_data[1:1 + num_bytes], "big")
-    return value
+    if cnt != (num_bytes + 1):
+        raise Exception(f"ReadError: cnt={cnt} (expected={num_bytes+1})")
+    return read_data[1:]
+
+def shift_right(n: int, shift: int, mask: BitMask = BitMask.SHORT) -> int:
+    u_n = n & mask
+    v1 = u_n >> shift
+
+
+def read_calibration_data(pi, spi_handler):
+    cal_1 = read_register(pi, spi_handler, 0x88, 24)
+    cal_2 = read_register(pi, spi_handler, 0xA1, 1)
+    cal_3 = read_register(pi, spi_handler, 0xE1, 7)
+    #print(f"0x88 ~ 0x9F: {bytes_to_binary(cal_1)}")
+    #print(f"0xA1: {bytes_to_binary(cal_2)}")
+    #print(f"0xE1 ~ 0xE7: {bytes_to_binary(cal_3)}")
+
+    #cnt = 0
+    #for b in cal_1:
+    #    print(f"{cnt}: {int_to_binary(b)}")
+    #    cnt += 1
+    #print(f"[0:2]: {bytes_to_binary(cal_1[0:2])}")
+
+    return OrderedDict([
+        # --- --- --- 0x88 ~ 0x9F --- --- ---
+        ("dig_T1", int.from_bytes(cal_1[0:2]  , byteorder="little", signed=False)),
+        ("dig_T2", int.from_bytes(cal_1[2:4]  , byteorder="little", signed=True)),
+        ("dig_T3", int.from_bytes(cal_1[4:6]  , byteorder="little", signed=True)),
+        ("dig_P1", int.from_bytes(cal_1[6:8]  , byteorder="little", signed=False)),
+        ("dig_P2", int.from_bytes(cal_1[8:10] , byteorder="little", signed=True)),
+        ("dig_P3", int.from_bytes(cal_1[10:12], byteorder="little", signed=True)),
+        ("dig_P4", int.from_bytes(cal_1[12:14], byteorder="little", signed=True)),
+        ("dig_P5", int.from_bytes(cal_1[14:16], byteorder="little", signed=True)),
+        ("dig_P6", int.from_bytes(cal_1[16:18], byteorder="little", signed=True)),
+        ("dig_P7", int.from_bytes(cal_1[18:20], byteorder="little", signed=True)),
+        ("dig_P8", int.from_bytes(cal_1[20:22], byteorder="little", signed=True)),
+        ("dig_P9", int.from_bytes(cal_1[22:24], byteorder="little", signed=True)),
+
+        # --- --- --- 0xA1 --- --- ---
+        ("dig_H1", int.from_bytes(cal_2, byteorder="little", signed=False)),
+
+        # --- --- --- 0xE1 ~ 0xE7 --- --- ---
+        ("dig_H2", int.from_bytes(cal_3[0:2], byteorder="little", signed=True)),
+        ("dig_H3", int.from_bytes(cal_3[2:3], byteorder="little", signed=False)),
+        #"dig_H4": cal_3[3] << 4 | (0b00001111 & cal_3[4])
+        ("dig_H4", int.from_bytes(bytes([cal_3[3], (0b00001111 & cal_3[4])]), byteorder="big", signed=True)),
+        #"dig_H5":  << 4 | (cal_3[4] >> 4)
+        ("dig_H5", int.from_bytes(bytes([cal_3[5], (0b00001111) & (cal_3[4] >> 4)]), byteorder="big", signed=True)),
+        ("dig_H6", int.from_bytes(cal_3[7:8], byteorder="little", signed=True)),
+    ])
+
+def read_temp(pi, spi_handler, cal_data: OrderedDict):
+    read_bytes = read_register(pi, spi_handler, 0xFA, 3)
+    # 温度は20ビットフォーマットで受信され、正値で32ビット符号付き整数
+    temp_raw = int.from_bytes(read_bytes, "big") >> 4
+    print(f"temp: bytes={bytes_to_binary(read_bytes)}, temp_raw={temp_raw}")
+
+    #var1 = ((((adc_T>>3) – ((BME280_S32_t)dig_T1<<1))) * ((BME280_S32_t)dig_T2)) >> 11;
+    var1 = (
+        (
+            (temp_raw >> 3) - (cal_data["dig_T1"] << 1)
+        ) * cal_data["dig_T2"]
+    ) >> 11
+
+    #var2 = (((((adc_T>>4) – ((BME280_S32_t)dig_T1)) * ((adc_T>>4) – ((BME280_S32_t)dig_T1))) >> 12) * ((BME280_S32_t)dig_T3)) >> 14;
+    var2 = (
+        (
+            (
+                (
+                    (temp_raw >> 4) - cal_data["dig_T1"]
+                ) * (
+                    (temp_raw >> 4) - cal_data["dig_T1"]
+                )
+            ) >> 12
+        ) * (
+            cal_data["dig_T3"]
+        )
+    ) >> 14
+
+    t_fine = var1 + var2
+    temp = ((t_fine * 5 + 128) >> 8) / 100
+    return temp
+
 
 
 def main(pi, spi_handler):
@@ -44,9 +133,18 @@ def main(pi, spi_handler):
     spi3w_en = 0  # 3-wire SPI Disable
     config_reg    = (t_sb << 5) | (filter << 2) | spi3w_en
     write_register(pi, spi_handler, 0xF5, config_reg)
+
+    # キャリブレーションデータ
+    cal_data = read_calibration_data(pi, spi_handler)
+    pprint(cal_data)
+
     while True:
-        result = read_register(pi, spi_handler, 0xFA, 3)
-        print(f"temp: {int_to_binary(result)}")
+        temp = read_temp(pi, spi_handler, cal_data)
+        print(f"temp: {temp}")
+        press = read_register(pi, spi_handler, 0xF7, 3)
+        print(f"press: {bytes_to_binary(press)}")
+        hum = read_register(pi, spi_handler, 0xFD, 2)
+        print(f"hum: {bytes_to_binary(hum)}")
         time.sleep(1)
 
 
