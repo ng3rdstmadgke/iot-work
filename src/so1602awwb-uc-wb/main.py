8784e9bca254bf2f540d14d9406874ec2b776099
ktamido <ng3rdstmadgke@gmail.com>
2023-09-18 15:01:41 +0900

    OLED

--- src/so1602awwb-uc-wb/main.py
+++ src/so1602awwb-uc-wb/main.py
@@ -2,6 +2,9 @@
 #   https://akizukidenshi.com/download/ds/akizuki/so1602awwb-uc-wb-u_akizuki_manu.pdf
 # データシート:
 #   https://akizukidenshi.com/download/ds/sunlike/SO1602AWWB-UC-WB-U.pdf
+#
+# LCDの使い方(AE-AQM1602A)基礎編:
+#   https://nobita-rx7.hatenablog.com/entry/27983030
 from time import sleep
 from typing import Union, List, Tuple
 import pigpio
@@ -18,49 +21,230 @@ def write_data(pi, i2c_handler, data: int):
 def write_command(pi, i2c_handler, command: int):
     control_byte = 0b00000000
     pi.i2c_write_device(i2c_handler, bytes([control_byte, command]))
-    sleep(0.01)
+    sleep(0.05)
 
 
 def init(pi, i2c_handler):
-    sleep(0.1)
-    write_command(pi, i2c_handler, 0b00000001)  # Clear Display
-    sleep(0.02)
-    write_command(pi, i2c_handler, 0b00000010)  # Return Home
-    sleep(0.002)
-    write_command(pi, i2c_handler, 0b00001111)  # Display ON
-    sleep(0.002)
-    write_command(pi, i2c_handler, 0b00000001)  # Clear Display
-    sleep(0.02)
+    # Clear Display (IS=X, RE=X, SD=0)
+    #   全DDRAMに0x20を書き込み、DDRAMを0x00(1行目先頭)に設定
+    #   0 0 0 0 0 0 0 1
+    write_command(pi, i2c_handler, 0b00000001)
+
+    # Return Home (IS=X, RE=0, SD=0)
+    #   DDRAMを0x00(1行目先頭)に設定
+    #   0 0 0 0 0 0 1 *
+    write_command(pi, i2c_handler, 0b00000010)
+
+    # Display ON/OFF Control
+    #   ディスプレイ・カーソル・ブリンクのON/OFFを設定
+    #   0 0 0 0 1 D C B
+    #   - D: ディスプレイ
+    #     (1) ON
+    #     (0) OFF (default)
+    #   - C: カーソル表示
+    #     (1) ON
+    #     (0) OFF (default)
+    #   - B: ブリンク表示
+    #     (1) ON
+    #     (0) OFF (default)
+    write_command(pi, i2c_handler, 0b00001100)
+
+    # Entry Mode Set (IS=0, RE=0, SD=0)
+    #   データ書き込み時のカーソルの移動方向の設定
+    #   0 0 0 0 0 1 I/D S
+    #   - I/D:
+    #     (1) カーソルが右に移動しDDRAMアドレスがインクリメント (default)
+    #     (0) カーソルが左に移動しDDRAMアドレスがデクリメント
+    #   - S: ディスプレイ全体のシフト
+    #     (1) I/D=1ならディスプレイ全体が右にシフト。I/D=0なら左にシフト。
+    #     (0) ディスプレイのシフトを行わない (default)
+    write_command(pi, i2c_handler, 0b00000110)  # シフト設定をデフォルト値に
+
+
+    # Function Set (IS=0, RE=1, SD=0)
+    #   機能設定
+    #   0 0 0 1 * N DH RE(0) REV
+    #   - N: 表示行数制御
+    #     (1) 2行表示
+    #     (0) 1行表示
+    #   - DH: 2行高フォント制御
+    #     (1) 2行分の高さで1文字を表示 (DDRAMは0x00 - 0x27まで利用可能)
+    #     (0) 通常表示
+    #   - RE: 拡張命令用レジスタ
+    #     (1) 利用する
+    #     (0) 利用しない
+    #   - REV: 表示反転
+    #     (1) 反転する
+    #     (0) 反転しない
+    write_command(pi, i2c_handler, 0b00101010)  # RE=1
+
+
+    # OLED Characterization (IS=0, RE=1, SD=0)
+    #   0 1 1 1 1 0 0 SD
+    #   - SD: 拡張命令用レジスタ
+    #     (1) 利用する
+    #     (0) 利用しない
+    write_command(pi, i2c_handler, 0b01111001)  # SD=1
+    # IS=0, RE=1, SD=1
+
+    # Set Contrast Control
+    #   コントラスト設定。
+    #   2バイトコマンドなので、このあとに輝度設定バイト(0x00 - 0xFF)を送信
+    #   1 0 0 0 0 0 0 1
+    write_command(pi, i2c_handler, 0b10000001)  # コントラストセット
+    write_command(pi, i2c_handler, 0b11111111)  # 輝度 max
+
+    # OLED Characterization (IS=0, RE=1, SD=0)
+    #   0 1 1 1 1 0 0 SD
+    #   - SD: 拡張命令用レジスタ
+    #     (1) 利用する
+    #     (0) 利用しない
+    write_command(pi, i2c_handler, 0b01111000)  # SD=0
+
+    # Function Set (IS=X, RE=0, SD=0)
+    #   機能設定
+    #   0 0 0 1 * N DH RE(0) IS
+    #   - N: 表示行数制御
+    #     (1) 2行表示
+    #     (0) 1行表示
+    #   - DH: 2行高フォント制御
+    #     (1) 2行分の高さで1文字を表示 (DDRAMは0x00 - 0x27まで利用可能)
+    #     (0) 通常表示
+    #   - RE: 拡張命令用レジスタ
+    #     (0) 利用しない
+    #   - IS: 拡張命令用レジスタ
+    #     (1) 利用する
+    #     (0) 利用しない
+    write_command(pi, i2c_handler, 0b00101000)  # RE=0, IS=0
+    # IS=0, RE=0, SD=0
+
+
 
 def off(pi, i2c_handler):
-    sleep(0.1)
+    write_command(pi, i2c_handler, 0b01111000)  # SD=1 (OLED Characterization)
+    write_command(pi, i2c_handler, 0b00101000)  # RE=0, IS=0 (Function Set)
     write_command(pi, i2c_handler, 0b00000001)  # Clear Display
-    sleep(0.02)
     write_command(pi, i2c_handler, 0b00000010)  # Return Home
-    sleep(0.002)
     write_command(pi, i2c_handler, 0b00001000)  # Display, cursor, blink = OFF
 
 
 
-def main(pi, i2c_handler):
+def disp_01(pi, i2c_handler):
+    """
+    固定表示
+    """
+    write_command(pi, i2c_handler, 0b00101000)  # bit2 (1: Double height, 0: Single height) (Function Set)
+    # IS=0, RE=0, SD=0
 
-    # 1文字書き込むとカーソルがインクリメントされる
-    text = b"24.34C 1012hPa"
-    for b in text:
-        write_data(pi, i2c_handler, b)
+    l1 = b"Temp  Pres Hum"
+    for char in l1:
+        write_data(pi, i2c_handler, char)
 
     ddram_addr = 0b00100000  # 2行目の先頭 (0x20)
     write_command(pi, i2c_handler, 0b10000000 | ddram_addr)  # Set DDRAM RAM Address
+    l2 = b"24.34 1012 50.35"
+    for ch in l2:
+        write_data(pi, i2c_handler, ch)  # 0xB1 = ア
 
-    for i in range(0, 16):
-        write_data(pi, i2c_handler, i + 0xB1)  # 0xB1 = ア
+    sleep(10)
+
+def disp_02(pi, i2c_handler):
+    """
+    繰り返し表示
+    """
+    # 設定
+
+
+    l1 = b"Temp  Pres Hum"  # 20byteまで保持できる
+    for char in l1:
+        write_data(pi, i2c_handler, char)
+
+    ddram_addr = 0b00100000  # 2行目の先頭 (0x20)
+    write_command(pi, i2c_handler, 0b10000000 | ddram_addr)  # Set DDRAM RAM Address
+    l2 = b"24.34 1012 50.35"
+    for ch in l2:
+        write_data(pi, i2c_handler, ch)  # 0xB1 = ア
     
-    write_command(pi, i2c_handler, 0b00101010)  # RE=1
-    write_command(pi, i2c_handler, 0b01111001)  # SD=1
-    write_command(pi, i2c_handler, 0b10000001)  # コントラストセット
-    write_command(pi, i2c_handler, 0b11111111)  # 輝度 max
-    write_command(pi, i2c_handler, 0b01111000)  # SD=0
-    write_command(pi, i2c_handler, 0b00101000)  # 2C=高文字 28=ノーマル
+    while (True):
+
+        # Cursor or Display Shift (IS=0, RE=0, SD=0)
+        #   カーソル位置の移動設定
+        #   0 0 0 1 S/C R/L * *
+        #   - S/C: 画面とカーソルどちらをシフトするかを選択
+        #     (1) 画面をシフト
+        #     (0) カーソルをシフト
+        #   - R/L: 左右どちらにシフトするかを選択
+        #     (1) 右にシフト
+        #     (0) 左にシフト
+        write_command(pi, i2c_handler, 0b00011000)
+        sleep(0.5)
+
+def disp_03(pi, i2c_handler):
+    l1 = b"Text:"
+    for char in l1:
+        write_data(pi, i2c_handler, char)
+    sleep(1)
+
+    # Entry Mode Set (IS=0, RE=0, SD=0)
+    #   データ書き込み時のカーソルの移動方向の設定
+    #   0 0 0 0 0 1 I/D S
+    #   - I/D:
+    #     (1) カーソルが右に移動しDDRAMアドレスがインクリメント (default)
+    #     (0) カーソルが左に移動しDDRAMアドレスがデクリメント
+    #   - S: ディスプレイ全体のシフト
+    #     (1) I/D=1ならディスプレイ全体が右にシフト。I/D=0なら左にシフト。
+    #     (0) ディスプレイのシフトを行わない (default)
+    write_command(pi, i2c_handler, 0b00000111)
+
+
+    l2 = b"The returned value is a tuple of the number of bytes read and a bytearray containing the bytes. If there was an error the number of bytes read will be less than zero (and will contain the error code)."
+    l2 += (b" " * (len(l2) % 20 + 20))
+    
+    for i, char in enumerate(l2):
+        if (i % 20 == 0):
+            # Set DDRAM RAM Address (IS=0, RE=X, SD=0)
+            #   DDRAMアドレスを設定
+            #   1 N N N N N N N
+            #   - N: アドレスを指定 1行目(0x00 - 0x0F) 2行目(0x20 - 0x2F)
+            write_command(pi, i2c_handler, 0b10100000)  # 2行目の先頭 (0x20)
+        write_data(pi, i2c_handler, char)
+        sleep(0.1)
+    
+
+def disp_04(pi, i2c_handler):
+    """
+    スクロール表示
+    """
+    # 設定
+
+    write_command(pi, i2c_handler, 0b00101010)  # RE=1 (Function Set)
+    # IS=0, RE=1, SD=0
+    write_command(pi, i2c_handler, 0b00011101)  # display_shift_enable (Double Height(4line)/Display-dot shift)
+
+    write_command(pi, i2c_handler, 0b00101001)  # RE=0, IS=1 (Function Set)
+    write_command(pi, i2c_handler, 0b00101010)  # RE=1 (Function Set)
+    # IS=1, RE=1, SD=0
+    #write_command(pi, i2c_handler, 0b00010010)  # enble_line2_shifrt (Display shift/scroll)
+
+    write_command(pi, i2c_handler, 0b00101000)  # RE=0, IS=0 (Function Set)
+    # IS=0, RE=0, SD=0
+
+    write_command(pi, i2c_handler, 0b00101000)  # bit2 (1: Double height, 0: Single height) (Function Set)
+    # IS=0, RE=0, SD=0
+
+    #ddram_addr = 0b00000000  # 1行目の先頭 (0x00)
+    #write_command(pi, i2c_handler, 0b10000000 | ddram_addr)  # Set DDRAM RAM Address
+    l1 = b"Temp  Pres Hum"
+    for char in l1:
+        write_data(pi, i2c_handler, char)
+
+    ddram_addr = 0b00100000  # 2行目の先頭 (0x20)
+    write_command(pi, i2c_handler, 0b10000000 | ddram_addr)  # Set DDRAM RAM Address
+    l2 = b"24.34 1012 50.35"
+    for ch in l2:
+        write_data(pi, i2c_handler, ch)  # 0xB1 = ア
+    
+    sleep(10)
 
 
 if __name__ == "__main__":
@@ -74,10 +258,12 @@ if __name__ == "__main__":
     i2c_handler = pi.i2c_open(i2c_bus, i2c_address)
     try:
         init(pi, i2c_handler)
-        main(pi, i2c_handler)
+        #disp_01(pi, i2c_handler)
+        #disp_02(pi, i2c_handler)
+        disp_03(pi, i2c_handler)
         sleep(10)
+    finally:
         off(pi, i2c_handler)
         sleep(1)
-    finally:
         pi.i2c_close(i2c_handler)
         pi.stop()
